8c132d34a4a435c2049a82636497c34a65d2ce2b
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-04-14 20:45:09 -0400

    Login Page Setup

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,28 @@
 import * as React from 'react';
-import { StyleSheet,View,Alert } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 import { Text, Button } from 'react-native-elements';
+import { NavigationScreenOptions, NavigationParams } from 'react-navigation';
 import { json } from '../utils/api'
+
+
+interface Props { }
+interface State { }
+
+
+export default class Login extends React.Component<Props, State> {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text h1 style={{ margin: 20 }}>Login Screen</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5FCFF'
+    }
+})
