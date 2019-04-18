bde11b6c745a30ecb74d6fb433409bd5ce69ce3a
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-04-18 02:18:19 -0400

    Added email and password input

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,20 +1,68 @@
 import * as React from 'react';
 import { StyleSheet, View, Alert } from 'react-native';
-import { Text, Button } from 'react-native-elements';
+import { Text, Button, Input } from 'react-native-elements';
 import { NavigationScreenOptions, NavigationParams } from 'react-navigation';
 import { json } from '../utils/api'
 
 
 interface Props { }
-interface State { }
+interface State {
+    email: string;
+    password: string;
+}
 
 
 export default class Login extends React.Component<Props, State> {
+    static navigationOptions: NavigationScreenOptions = {
+        headerTitle: "Login"
+    };
 
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    async handleLogin() {
+        try {
+            let result = await json('https://cherry-pie-19862.herokuapp.com//auth/login', 'POST', {
+                email: 'KyledaNovice@hooblah.com',
+                password: 'Blogapp'
+            })
+            console.log(result)
+        } catch (e) {
+            console.log(e)
+            Alert.alert('Problem Logging In. Contact Admin!!')
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text h1 style={{ margin: 20 }}>Login Screen</Text>
+                <View style={{ flex: 1 }}>
+                    <Input value={this.state.email}
+                    leftIcon={{type: 'font-awesome',name: 'envelope'}}
+                        placeholder="Email"
+                        onChangeText={(text) => this.setState({ email: text })}
+                    />
+
+                    <Input value={this.state.password}
+                        placeholder="Password"
+                        leftIcon={{type: 'font-awesome',name: 'key'}}
+                        onChangeText={(text) => this.setState({ password: text })} />
+                </View>
+
+                <View style={{ flex: 1 }}>
+                    <Button
+                        raised
+                        title="Login"
+                        containerStyle={{ margin: 10 }}
+                        buttonStyle={{ backgroundColor: '#AE3CD7' }}
+                        // onPress={() => this.handleLogin()} 
+                        onPress={() => console.log('Test!')}
+                    />
+                </View>
             </View>
         )
     }
