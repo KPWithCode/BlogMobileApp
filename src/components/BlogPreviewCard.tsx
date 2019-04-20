6bdfb11cb8141c776deef43fc7854bab243bb87e
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-04-20 18:39:18 -0400

    Preview Card for Blog

--- src/components/BlogPreviewCard.tsx
+++ src/components/BlogPreviewCard.tsx
@@ -28,7 +28,7 @@ class BlogPreviewCard extends React.Component<Props, State> {
             <Card
                 title={title}
                 image={{ uri: 'https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80' }}>
-                <Text style={{ marginBottom: 10 }}>{`Written by:\n\n${firstname} ${lastname}`} </Text>
+                <Text style={{ marginBottom: 10 }}>{`Written by: Kyle Phillips`} </Text>
                 <Button
                     icon={<Icon name='code' color='#ffffff' />}
                     buttonStyle={{ backgroundColor: '#AE3CD7', borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
